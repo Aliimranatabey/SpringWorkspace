36a5039021c11421b62539ca10fbbe64e4b7d91f
Aliimranatabey <aliimranatabey@gmail.com>
2023-07-01 17:13:13 +0300

    worked with @RequestParam

--- lesson1/src/main/java/com/turkishjavadeveloper/controller/MyController.java
+++ lesson1/src/main/java/com/turkishjavadeveloper/controller/MyController.java
@@ -1,23 +1,31 @@
 package com.turkishjavadeveloper.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api")
 public class MyController {
     //Asagidaki iki methot ayni anlama gelirler .
     //@PathVariable : Kullanıcı bir mesaj girecek ve biz o mesajı kullanıcıya geri dönderecegiz.
-    @GetMapping(path = "/messages/{message}")
+    @GetMapping(path = "/PathVariable/messages/{message}")
     // {} icerisine "message" degil de "m" yazsaydık eslestirmeyi yapamayacaktı ve Server Error alacaktık . Ancak bu sekilde yapmak istiyorsak bir alttaki methot buna bir ornektir .
     public String getMyMessage(@PathVariable String message) {
         return "Your message is : " + message;
     }
 
-    @GetMapping(path = "/message/{m}")
+    @GetMapping(path = "/PathVariable/message/{m}")
     public String getMyMessage2(@PathVariable("m") String message) {
+        return "Your message is : " + message;
+    }
+
+    @GetMapping(path = "/RequestParam/messages")
+    public String getMyMessage3(@RequestParam String message) {
+        return "Your message is : " + message;
+    }
+
+    //RequestParam("") içerisine girilen deger her neyse postmande key kısmına o yazılır . Eğer RequestParam("") içerisinde hicbirsey yazmıyorsa bir üst methottaki gibi yani , direkt String ile tanımlanan değişken ismi key kısmına yazılır.
+    @GetMapping(path = "/RequestParam/message")
+    public String getMyMessage4(@RequestParam("m") String message) {
         return "Your message is : " + message;
     }
 }
